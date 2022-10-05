ed6c171c946e7e9ddcb4cdccec3f07babf1bf471
Alexander Chernykh <ytipytu1998@gmail.com>
2022-10-06 00:55:01 +0300

    Update practice_1.java

--- practices/practice_1/practice_1.java
+++ practices/practice_1/practice_1.java
@@ -1,1 +1,51 @@
-lol
+class Book {
+    private String title;
+    private int date_of_publication;
+
+    public Book() {
+        title = "unknown";
+        date_of_publication = 0;
+    }
+
+    public Book(String title) {
+        this.title = title;
+        date_of_publication = 0;
+    }
+
+    public Book(String title, int date_of_publication) {
+        this.title = title;
+        this.date_of_publication = date_of_publication;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDate_of_publication(int date_of_publication) {
+        this.date_of_publication = date_of_publication;
+    }
+
+    public int getDate_of_publication() {
+        return date_of_publication;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String toString(){
+        return this.title+" "+this.date_of_publication;
+    }
+
+}
+
+public class TestBook {
+    public static void main(String[] args) {
+        Book book_1 = new Book();
+        Book book_2 = new Book("LOL");
+        Book book_3 = new Book("NOT LOL", 1965);
+        System.out.println(book_1);
+        System.out.println(book_2);
+        System.out.println(book_3);
+    }
+}
