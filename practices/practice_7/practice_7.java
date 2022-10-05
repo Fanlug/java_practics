b301fd2111f276984795afeafb9f46e585119ee5
Alexander Chernykh <ytipytu1998@gmail.com>
2022-10-06 02:03:24 +0300

    Update practice_7.java

--- practices/practice_7/practice_7.java
+++ practices/practice_7/practice_7.java
@@ -1,1 +1,28 @@
+import java.util.Stack;
 
+public class Main {
+    public static String work(int[] word) {
+        StringBuilder res = new StringBuilder();
+        Stack<Integer> wordArray = new Stack<>();
+        for (int j =  0; j<word.length;  j++) {
+            if (word[j] > 0) {
+                wordArray.add(word[j]);
+                res.append("0");
+            } else if (word[j] == 0) {
+                if (wordArray.isEmpty()) {
+                    res.append("-1");
+                } else {
+                    res.append((int) wordArray.pop());
+                }
+            }
+            if (word.length!=1 && j!=word.length-1)
+                res.append(",");
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        int[] word = new int[]{0, 1, 8, 4, 0, 3, 0, 8, 8, 3, 7};
+        System.out.println(work(word));
+    }
+}
