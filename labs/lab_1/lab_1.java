9046ed8d162ed6df94ece3f3672d4384d2633a3d
Alexander Chernykh <ytipytu1998@gmail.com>
2022-10-06 00:14:25 +0300

    Update lab_1.java

--- labs/lab_1/lab_1.java
+++ labs/lab_1/lab_1.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class Main {
+    static long factorial(int number){
+        long result = 1;
+        for (int i = 1; i < number; i++) {
+            result*=i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(factorial(scanner.nextInt()));
+    }
+
+}
