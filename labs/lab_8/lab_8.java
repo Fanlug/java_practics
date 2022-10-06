a7d92985d8dda78115156de05923dd4eb87a25aa
Alexander Chernykh <ytipytu1998@gmail.com>
2022-10-06 17:48:02 +0300

    Update lab_8.java

--- labs/lab_8/lab_8.java
+++ labs/lab_8/lab_8.java
@@ -1,1 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scannerSys = new Scanner(System.in);
+        File file = new File(scannerSys.next());
+        Scanner scannerFile = new Scanner(file);
+        while (scannerFile.hasNextLine())
+            System.out.println(scannerFile.nextLine());
+    }
+}
